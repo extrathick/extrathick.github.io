e6a1b5375d7ba659cd5aedc9ce94fe31b0ee8a5e
Justin Pham <jpham79@hawaii.edu>
2018-11-07 14:26:39 -1000

    test2

---
+++ precache-manifest.a120dba9a0bd3d02a990275e42fd10ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1d832a9df33a1166efd6",
+    "url": "/static/js/main.1d832a9d.chunk.js"
+  },
+  {
+    "revision": "4f909ea95cf1e1b93f43",
+    "url": "/static/js/1.4f909ea9.chunk.js"
+  },
+  {
+    "revision": "1d832a9df33a1166efd6",
+    "url": "/static/css/main.d268fa19.chunk.css"
+  },
+  {
+    "revision": "d845d701e8d30537462ad36da3f80ed8",
+    "url": "/index.html"
+  }
+];
